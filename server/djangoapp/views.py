a730fdd37562bf6538baf5ee9848b5811d5aaaf0
Khang Zhie Phoong <phoongkz@gmail.com>
2025-03-20 01:54:13 -0400

    Done with mod 4

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -82,7 +82,7 @@ def registration(request):
 # Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if(state == "All"):
-        endpoint = "/fetchDealers"
+        endpoint = "/fetchDealers/"
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,4 +1,7 @@
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
 import LoginPanel from "./components/Login/Login"
+import PostReview from "./components/Dealers/PostReview"
 import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 
@@ -7,6 +10,9 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterPanel />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
